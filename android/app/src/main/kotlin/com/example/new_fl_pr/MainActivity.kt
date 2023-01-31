f2b4195fc0509b72fb2873a4aff268ed08040abf
Artamonova T.D <odettnix@gmail.com>
2023-01-31 16:08:28 +0300

    new pr

---
+++ android/app/src/main/kotlin/com/example/new_fl_pr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.new_fl_pr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/user.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.g.dart';
+part 'user.freezed.dart';
+
+@freezed
+class User with _$User{
+
+  const factory User({
+    required String userName,
+    required String email,
+    required String password,
+  }) = _User;
+
+  factory User.fromJson(Map<String,dynamic> json) => _$UserFromJson(json);
+  
+}
